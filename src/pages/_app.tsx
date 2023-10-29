f1299e8714ddab61539d39aba63457c90d0b3096
Jun99uu_Lee <igun0423@naver.com>
2023-10-29 17:52:51 +0900

    ➕ Add tanstack query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { Frame } from "@/components/Layouts";
 import { Global } from "@emotion/react";
 import reset from "@/styles/reset";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({
   Component,
@@ -11,10 +14,12 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Frame>
-        <Global styles={reset} />
-        <Component {...pageProps} />
-      </Frame>
+      <QueryClientProvider client={queryClient}>
+        <Frame>
+          <Global styles={reset} />
+          <Component {...pageProps} />
+        </Frame>
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
